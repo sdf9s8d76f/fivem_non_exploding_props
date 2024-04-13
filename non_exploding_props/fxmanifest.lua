fc7971fbc0128e275d2cf766ac368aac6296a559
Nick <83879796+sdf9s8d76f@users.noreply.github.com>
2024-04-14 00:46:32 +0200

    restructure

--- non_exploding_props/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version "cerulean"
-game "gta5"
-
-author "nick__."
-description "Removes explosion effect from following props: prop_gas_tank_01a, prop_gas_tank_02a, prop_gas_tank_02b, prop_gas_tank_04a, prop_gascage01, prop_gascyl_01a, prop_gascyl_02a, prop_gascyl_02b, prop_gascyl_03a, prop_gascyl_03b, prop_gascyl_04a, prop_oilcan_01a, prop_oilcan_02a, prop_storagetank_02b, prop_tanktrailer_01a"
-version "1.0.0"
-
-data_file "DLC_ITYP_REQUEST" "stream/v_industrial.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/v_storage.ytyp"
